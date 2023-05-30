488aaea09c30ce6fe8873622fda2853601d33cc5
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-05-30 12:36:21 -0300

    commit 2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import Head from 'next/head';
-import { Inter } from 'next/font/google';
+import Image from 'next/image';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
-
-const inter = Inter({ subsets: ['latin'] });
+import { Button, Card, CardBody, CardText, Col, Row, CardHeader, CardFooter } from 'reactstrap';
+import styles from '@/styles/Home.module.css'
+import logo_web from "@/assets/images/logo_web.png";
+import imagem from "@/assets/images/imagem.png";
+import { BsArrowUpRight } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -14,12 +16,57 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>
-          <Button color="primary">Click Me</Button>
+      <main className="d-flex flex-column">
+        <div className="d-flex justify-content-center align-items-center pt-3 pb-3">
+          <p className="mb-0 me-2">Não limite sua criatividade, junte-se a familia Blocks por apenas <span className="fw-bold">BRL 19,99</span></p>
+          <Button>Quero ser Premium</Button>
+        </div>
+        <div className="ps-4 pe-4">
+          <section className="d-flex justify-content-center align-items-center">
+            <Image src={logo_web} alt="logo_web" />
+          </section>
+          <section className="d-flex justify-content-start align-items-center">
+            <h1>Catálogo</h1>
+          </section>
+          <section>
+            <h2>Resultados</h2>
+            <Row>
+              <ItemResultadoCatalogo></ItemResultadoCatalogo>
+              <ItemResultadoCatalogo></ItemResultadoCatalogo>
+              <ItemResultadoCatalogo></ItemResultadoCatalogo>
+              <ItemResultadoCatalogo></ItemResultadoCatalogo>
+              <ItemResultadoCatalogo></ItemResultadoCatalogo>
+              <ItemResultadoCatalogo></ItemResultadoCatalogo>
+              <ItemResultadoCatalogo></ItemResultadoCatalogo>
+            </Row>
+          </section>
+          <footer>
+            <ul>
+              <li>Sobre</li>
+              <li>FAQ</li>
+              <li>Termos de uso</li>
+              <li>Politica de privacidade</li>
+            </ul>
+          </footer>
         </div>
       </main>
     </>
+  );
+}
+
+function ItemResultadoCatalogo() {
+  return (
+    <Col className="mb-4">
+      <Card style={{ width: '18rem' }}>
+        <CardBody className="d-flex justify-content-center align-items-center">
+          <Image src={imagem} alt="imagem" />
+        </CardBody>
+        <CardFooter className="d-flex justify-content-center align-items-center">
+          <CardText className="border-end border-dark me-2 mb-0">Lorem Ipsum is sLorem Ipsum is simply ....</CardText>
+          <BsArrowUpRight />
+        </CardFooter>
+      </Card>
+    </Col>
   );
 }
 
