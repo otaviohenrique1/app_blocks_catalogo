d398f2cd5fcfe307c7df6a4dcff164f913a98dc4
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-05-30 20:44:12 -0300

    commit 14

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'plugin-storage.nyc3.digitaloceanspaces.com',
+        port: '',
+        // pathname: '/account123/**',
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -141,29 +141,15 @@ export default function Home() {
             <h2 className={`${styles.resultados_titulo} ${open_sans.className}`}>Resultados</h2>
             <Row className="p-0 m-0">
               {data.map((item, index) => {
-                return <p key={item.id.toString()}>{item.id.toString()}</p>;
+                return (
+                  <ItemResultadoCatalogo
+                    key={item.id}
+                    id={item.id}
+                    details={item.details}
+                    premium={item.premium}
+                  />
+                );
               })}
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              {/* <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo>
-              <ItemResultadoCatalogo></ItemResultadoCatalogo> */}
             </Row>
           </section>
           <footer className={`d-flex ${styles.rodape}`}>
@@ -179,15 +165,38 @@ export default function Home() {
   );
 }
 
-function ItemResultadoCatalogo() {
+/*
+  http://localhost:8080/families*
+  http://localhost:8080/families?skip=0&take=10
+  http://localhost:8080/families?skip=10&take=10
+  https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/ID DA FAMÍLIA.jpg
+  https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/abc-123.jpg
+*/
+
+type ItemResultadoCatalogoTypes = DataListType;
+
+function ItemResultadoCatalogo(props: ItemResultadoCatalogoTypes) {
+  const { id, premium, details } = props;
+  const { name, description } = details;
+
   return (
     <Col className={`p-0 m-0 mb-3 ${styles.card_col}`}>
       <Card className={`${styles.card_container}`}>
         <CardBody className="d-flex justify-content-center align-items-center">
-          <Image src={imagem} alt="imagem" className="img-fluid" />
+          <Image
+            src={`https://plugin-storage.nyc3.digitaloceanspaces.com/families/images/${id}.jpg`}
+            alt="imagem"
+            className="img-fluid"
+            width={100}
+            height={50}
+          />
+          {/* <Image src={imagem} alt="imagem" className="img-fluid" /> */}
         </CardBody>
         <CardFooter className="d-flex justify-content-center align-items-center">
-          <CardText className={`mb-0 ${open_sans.className} ${styles.card_texto}`}>Lorem Ipsum is sLorem Ipsum is simply ....</CardText>
+          <CardText className={`mb-0 ${open_sans.className} ${styles.card_texto}`}>
+            {name}
+            {/* Lorem Ipsum is sLorem Ipsum is simply .... */}
+          </CardText>
           <span className={`me-2 ms-2 ${styles.cardtext_separator}`} />
           <BsArrowUpRight color='#474747' size={16} className={styles.card_icone_botao} />
         </CardFooter>
